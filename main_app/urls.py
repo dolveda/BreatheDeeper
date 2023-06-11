d98dd123218bfea24af3fb42328ec484de639924
dolveda <dolveda0@gmail.com>
2023-06-11 16:58:36 -0700

    Update profile page to profiles index, add card to profiles.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('profile/', views.profile, name='profile'),
+    path('profiles/', views.profiles_index, name='profiles'),
     path('cities/', views.cities_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import Profile, City
 
 def home(request):
     return render(request, 'home.html')
 
-def profile(request):
-    return render(request, 'profile.html')
+def profiles_index(request):
+    profiles = Profile.objects.all()
+    return render(request, 'profiles/profiles.html', {'profiles': profiles})
 
 def cities_index(request):
     return render(request, 'cities/index.html')
