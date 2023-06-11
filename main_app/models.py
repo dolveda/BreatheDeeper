d987bf8095a69bc8b2a8d6b976d62a34306b4a40
dolveda <dolveda0@gmail.com>
2023-06-11 12:46:27 -0700

    Create Log Model

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,21 @@ class User(models.Model):
 class City(models.Model):
     name = models.CharField()
     aqi = models.IntegerField()
+
+class Log(models.Model):
+    OPTIONS = (
+        ('B', 'Breathe Deeper'),
+        ('D', "Don't Inhale"),
+        ('M', 'Mask Up')
+    )
+    date = models.DateField('Date')
+    option = models.CharField(
+        max_length=1,
+        choices=OPTIONS,
+        default=OPTIONS[0][0]
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_option_display()} on {self.date}"
