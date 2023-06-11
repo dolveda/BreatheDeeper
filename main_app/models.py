8e05994308fb272dd15dcd383160f19a280b5a52
dolveda <dolveda0@gmail.com>
2023-06-11 12:30:48 -0700

    Create City Model

--- main_app/models.py
+++ main_app/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=25)
-    
+
+class City(models.Model):
+    name = models.CharField()
+    aqi = models.IntegerField()
